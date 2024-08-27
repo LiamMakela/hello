3baa3b38f91dfd8f293e50304c5f5d30404385b4
Liam Makela <lmakela2@huskers.unl.edu>
2024-08-27 11:10:33 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Liam Makela
+ *         lmakela2@huskers.edu.unl
+ * Date: 2024/08/27
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Liam Makela\n");
+  return 0;
+}
