dbcb60c875244650506a9139a059c4879a0ad9c9
asmith183 <asmith183@huskers.unl.edu>
2024-08-28 18:01:48 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Aden Smith
+ * Date: 2024/08/28
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Aden Smith\n");
+
+    return 0;
+}
