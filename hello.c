ef8052312f831d1acab18bdac06920c7bfe9b82e
Liam Makela <lmakela2@huskers.unl.edu>
2024-08-29 11:41:46 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -9,8 +9,9 @@
 int main(int argc, char **argv) {
 
     printf("Hi my name is Aden Smith!\n");
+    printf("Hi my name is Liam Makela!\n");
 
-    printf("I'm a Computer Science Major\n");
-
+    printf("We are Computer Science Majors\n");
+    printf("(:\n");
     return 0;
 }
